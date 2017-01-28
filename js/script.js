3df4f49a9c63e373795b11b4a80cf639bc09387d
thibault <thibault.daboville@gmail.com>
2017-01-28 19:43:32 +0100

    commit avant création nouvelle branche

--- js/script.js
+++ js/script.js
@@ -1,25 +1,38 @@
-$(document).ready(
-    function()
-    {
-        $.ajax( {
-            type: "GET",
-            url: "data.xml",
-            dataType: "xml",
-            success: function(xml)
-            {
-                $(xml).find('client').each( function()
-                {
-                    var id = $(this).attr('id');
-                    var nom = $(this).find('nom').text();
+$(document).ready(function(){
+                console.log("coucou");
+                var x;
+                var id;
+                var nom;
+                var tabnum = [];
 
-                    console.log(id);
-                    $('.choix').append($('<option>',{ value: id, text: nom}));
+                $.ajax({
+                                type: "GET",
+                                url: "xml.xml",
+                                dataType: "xml",
+                                success: xmlFunction,
+                        });
+        });
 
-                } );
+function xmlFunction(xml) {
+        $("select").change(function(){
+                        var foo = $.parseXML(xml);
+                        //nom = $(xml).find("clients").find("client").find('nom').html();
+                        //console.log($(nom));
+                        console.log('coucou2');
+                });
 
-            }
-            $('.choix').change($('<option>',{ value: id, text: nom }));
-        }
-        );
-    }
-    );
+        $(xml).find('clients').each(function() {
+                        x = $(this).attr('id');
+                        tabnum.push(x);
+                        nom = $(this).find('nom').text();
+                        console.log(x);
+                        $(".choix").append($('<option>', { value: x, text: nom }));
+                        console.log('func');
+                });
+}
+
+//
+// getelementbytagname
+//
+// firstchild.nodevalue
+//
